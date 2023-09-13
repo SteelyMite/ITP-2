a2b05d1f6531197b16a7a672cb636dd66ff432a6
SteelyMite <NoContact@gmail.com>
2023-09-13 10:52:49 +1000

    Worked on Data Analysis (Clustering + Classification)

--- analysisFunctions.py
+++ analysisFunctions.py
@@ -4,13 +4,100 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, silhouette_score
 import dash
-from dash import dcc
-# import dash_core_components as dcc
-# import dash_html_components as html
-from dash import html
+import dash_core_components as dcc
+import dash_html_components as html
 import plotly.graph_objs as go
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
+from dash.dependencies import Input, Output
+from sklearn.preprocessing import StandardScaler
+
+# Create a Dash app
+app = dash.Dash(__name__)
+
+# Define the layout of the Dash app
+app.layout = html.Div([
+    html.H1('Predictive Statistics Dashboard'),
+
+    # Dropdown to select the method
+    dcc.Dropdown(
+        id='method-selector',
+        options=[
+            {'label': 'Classification', 'value': 'classification'},
+            {'label': 'Clustering', 'value': 'clustering'},
+        ],
+        value='classification'  # Default method
+    ),
+
+    # Input components for method-specific parameters
+    dcc.Input(
+        id='target-column-input',
+        type='text',
+        placeholder='Enter target column for classification',
+        style={'display': 'none'}  # Hide by default
+    ),
+
+    dcc.Input(
+        id='num-clusters-input',
+        type='number',
+        placeholder='Enter number of clusters for clustering',
+        style={'display': 'none'}  # Hide by default
+    ),
+
+    # Button to trigger method execution
+    html.Button('Run', id='run-button'),
+
+    html.H2(id='classification-accuracy', style={'display': 'none'}),
+    html.H2(id='cluster-silhouette-score', style={'display': 'none'}),
+
+    dcc.Graph(
+        id='cluster-plot',
+        style={'display': 'none'}  # Hide by default
+    ),
+])
+
+
+@app.callback(
+    Output('target-column-input', 'style'),
+    Output('num-clusters-input', 'style'),
+    Output('cluster-plot', 'style'),
+    Output('classification-accuracy', 'style'),
+    Output('cluster-silhouette-score', 'style'),
+    Input('method-selector', 'value')
+)
+def show_hide_inputs(method):
+    if method == 'classification':
+        return {'display': 'block'}, {'display': 'none'}, {'display': 'none'}, {'display': 'none'}, {'display': 'none'}
+    else:
+        return {'display': 'none'}, {'display': 'block'}, {'display': 'block'}, {'display': 'block'}, {'display': 'block'}
+
+
+@app.callback(
+    Output('classification-accuracy', 'children'),
+    Output('cluster-silhouette-score', 'children'),
+    Output('cluster-plot', 'figure'),
+    Input('run-button', 'n_clicks'),
+    Input('method-selector', 'value'),
+    Input('target-column-input', 'value'),
+    Input('num-clusters-input', 'value'),
+)
+def perform_analysis(n_clicks, method, target_column, num_clusters):
+    if n_clicks is None:
+        return '', '', {'data': []}
+
+    data = pd.read_csv('social_capital.csv')
+
+    if method == 'classification':
+        if not target_column:
+            return 'Target column not specified', '', {'data': []}
+        classification_accuracy = perform_classification(data, target_column)
+        return 'Classification Accuracy: {:.2f}'.format(classification_accuracy), '', {'data': []}
+    else:
+        if not num_clusters:
+            return '', 'Number of clusters not specified', {'data': []}
+        cluster_silhouette_score, scatter_plot = perform_clustering(data, num_clusters)
+        return '', 'Cluster Silhouette Score: {:.2f}'.format(cluster_silhouette_score), scatter_plot
+
 
 def perform_classification(data, target_column):
     """
@@ -53,9 +140,6 @@ def perform_classification(data, target_column):
 
     return classification_accuracy
 
-
-from sklearn.preprocessing import StandardScaler
-
 def perform_clustering(data, num_clusters=3):
     """
     Perform clustering on a Pandas DataFrame.
@@ -78,7 +162,7 @@ def perform_clustering(data, num_clusters=3):
     X_scaled = scaler.fit_transform(numeric_data)
 
     # Perform Clustering using K-Means
-    kmeans = KMeans(n_clusters=num_clusters, n_init=10, random_state=42)
+    kmeans = KMeans(n_clusters=num_clusters, random_state=42)
     cluster_labels = kmeans.fit_predict(X_scaled)
 
     # Calculate cluster silhouette score (a measure of clustering quality)
@@ -109,30 +193,7 @@ def perform_clustering(data, num_clusters=3):
     return cluster_silhouette_score, scatter_plot
 
 
-# Create a Dash app
-app = dash.Dash(__name__)
 
-# Example usage:
+# Run the Dash app
 if __name__ == "__main__":
-    data = pd.read_csv('social_capital.csv')
-
-    # Specify the target column for classification
-    target_column = 'YEAR'
-
-    classification_accuracy = perform_classification(data, target_column)
-
-    cluster_silhouette_score, scatter_plot = perform_clustering(data)
-
-    # Define the layout of the Dash app
-    app.layout = html.Div([
-        html.H1('Predictive Statistics Dashboard'),
-
-        html.H2('Classification Accuracy: {:.2f}'.format(classification_accuracy)),
-
-        html.H2('Cluster Silhouette Score: {:.2f}'.format(cluster_silhouette_score)),
-
-        scatter_plot  # Display the Dash graph
-    ])
-
-    # Run the Dash app
     app.run_server(debug=True)
