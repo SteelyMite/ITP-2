380b80476fdd3894c2bcfa00a5fab1e8786b6972
Pat-chiti <patm2000m@gmail.com>
2023-10-05 12:45:20 +1100

    Update parameter generator

--- parameterGenerator.py
+++ parameterGenerator.py
@@ -23,19 +23,19 @@ app.layout = html.Div([
         {
             "Input": "number_selection",
             "DataType": "numeric",
-            "Text": "4. Choose a number:",
+            "Text": "4. Choose a number 2-5:",
             "AcceptableValues": {"min": 2, "max": 5}
         },
         {
             "Input": "column_selection",
             "DataType": "both",
-            "Text": "3. Choose a column:",
+            "Text": "3. Choose 3 columns:",
             "AcceptableValues": 3
         },
         {
             "Input": "column_selection",
             "DataType": "string",
-            "Text": "2. Choose a column:",
+            "Text": "2. Choose 2 columns:",
             "AcceptableValues": 2
         },
         {
@@ -44,20 +44,35 @@ app.layout = html.Div([
             "Text": "1. Choose a parameter:",
             "AcceptableValues": ["linear", "multilinear", "polynomial"]
         },
-        
-        
-        
         {
             "Input": "number_selection",
             "DataType": "numeric",  
-            "Text": "5. Select a number:",
+            "Text": "5. Select a number up to 10:",
             "AcceptableValues": {"min": None, "max": 10}
         },
         {
             "Input": "number_selection",
             "DataType": "numeric",  
-            "Text": "6. Select a number:",
+            "Text": "6. Select any number:",
             "AcceptableValues": {"min": None, "max": None}
+        },
+        {
+            "Input": "column_selection",
+            "DataType": "both",  
+            "Text": "7. Choose 1-3 columns:",
+            "AcceptableValues": {"min": 1, "max": 3}
+        },
+        {
+            "Input": "column_selection",
+            "DataType": "both",  
+            "Text": "8. Choose 1 or more columns:",
+            "AcceptableValues": {"min": 1, "max": None}
+        },
+        {
+            "Input": "column_selection",
+            "DataType": "both",  
+            "Text": "8. Choose up to 3 columns:",
+            "AcceptableValues": {"min": None, "max": 3}
         }
     ]),
     html.Button('Start', id='start-button'),
@@ -84,9 +99,6 @@ app.layout = html.Div([
 )
 def generate_input(n_clicks, input_dicts, df_data):
     # Only generate inputs when the button is clicked
-    print(f"Button clicked {n_clicks} times")
-    print(f"Input Dicts: {input_dicts}")
-    print(f"DF Data: {df_data}")
     if n_clicks is None or n_clicks == 0:
         return []
     
@@ -130,7 +142,7 @@ def generate_input(n_clicks, input_dicts, df_data):
             
             components.append(dcc.Dropdown(
                 options=options,
-                multi=(acceptable_value > 1)
+                multi= True
             ))
             components.append(html.Button('Test', id={'type': 'test-button', 'index': input_dicts.index(input_dict)}))
             components.append(html.Div(id={'type': 'test-result-div', 'index': input_dicts.index(input_dict)}))
@@ -175,8 +187,23 @@ def test_input(n_clicks, button_id, dynamic_input_children, input_dicts):
         return "True"
     elif input_type == "number_selection" and input_component['props']['value'] is not None:
         return "True"
-    elif input_type == "column_selection" and input_component['props']['value'] is not None and len(input_component['props']['value']) == acceptable_value:
-        return "True"
+    elif input_type == "column_selection" and input_component['props']['value'] is not None:
+        num_selected_columns = len(input_component['props']['value'])
+        
+        # Check if acceptable_value is an integer or a dictionary
+        if isinstance(acceptable_value, int):
+            # If it's an integer, check for exact match
+            return "True" if num_selected_columns == acceptable_value else "False"
+        elif isinstance(acceptable_value, dict):
+            # If it's a dictionary, check for range match
+            min_columns = acceptable_value.get("min", 0)  # Default to 0 if not provided
+            max_columns = acceptable_value.get("max", num_selected_columns)  # Default to num_selected_columns if not provided
+            
+            # If min or max is None, replace with num_selected_columns for no limit
+            min_columns = num_selected_columns if min_columns is None else min_columns
+            max_columns = num_selected_columns if max_columns is None else max_columns
+            
+            return "True" if min_columns <= num_selected_columns <= max_columns else "False"
     else:
         return "False"
 
@@ -203,12 +230,30 @@ def perform_operation(n_clicks, dynamic_input_children, input_dicts):
         if 'value' not in input_component['props'] or input_component['props']['value'] is None:
             return "Missing input or something, try again."
         
-        if input_type == "column_selection" and len(input_component['props']['value']) != acceptable_value:
-            return "Missing input or something, try again."
+        if input_type == "column_selection":
+            num_selected_columns = len(input_component['props']['value'])
+            
+            # Check if acceptable_value is an integer or a dictionary
+            if isinstance(acceptable_value, int):
+                # If it's an integer, check for exact match
+                if num_selected_columns != acceptable_value:
+                    return "Missing input or something, try again."
+            elif isinstance(acceptable_value, dict):
+                # If it's a dictionary, check for range match
+                min_columns = acceptable_value.get("min", 0)  # Default to 0 if not provided
+                max_columns = acceptable_value.get("max", num_selected_columns)  # Default to num_selected_columns if not provided
+                
+                # If min or max is None, replace with num_selected_columns for no limit
+                min_columns = num_selected_columns if min_columns is None else min_columns
+                max_columns = num_selected_columns if max_columns is None else max_columns
+                
+                if not (min_columns <= num_selected_columns <= max_columns):
+                    return "Missing input or something, try again."
     
-    # Perform desired operations here
+    # Perform your desired operations here
     # ...
     return "Operation performed successfully!"
+
 
 
 
