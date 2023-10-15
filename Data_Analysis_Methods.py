903830b1742f7b8f67326be62be41455c0bc7c2d
SteelyMite <NoContact@gmail.com>
2023-10-15 22:08:02 +1100

    Added Strings to Data Analysis methods for Script Export

--- Data_Analysis_Methods.py
+++ Data_Analysis_Methods.py
@@ -49,7 +49,41 @@ def clustering_KMeans(inputData, selectedColumns,numClusters):
         bar_plot.update_layout(title='K-means Clustering - Cluster Profile')
         fig.append(bar_plot)
         fig.append(pair_plot)
+
+    # Export Script
+    exportScript = """
+    fig = []
+    kmeans = KMeans(n_clusters=numClusters)
+    selectedData = inputData[selectedColumns]
+    cluster_assignments = kmeans.fit_predict(selectedData)
+    #? Create data analysis HTML report
+    cluster_centers = kmeans.cluster_centers_
+    inertia = kmeans.inertia_
+
+
+    statistics = html.Div([
+        html.H4("Cluster Statistics:"),
+        html.P(f"Number of Clusters (K): {numClusters}"),
+        html.P(f"Cluster Centers:\n{cluster_centers}"),
+        html.P(f"Inertia (Within-cluster Sum of Squares): {inertia}")
+    ])
+    #? Generate Plot if 2D, Else return Scatter Matrix and Cluster Profile
+    if(len(selectedColumns)==2):
+        scatter_plot = px.scatter(selectedData, x=selectedColumns[0], y=selectedColumns[1], color=cluster_assignments, title='K-means Clustering')
+        scatter_plot.update_layout(title='K-means Clustering - Scatter Plot')
+        fig.append(scatter_plot)
+    else:
+        pair_plot = px.scatter_matrix(selectedData, dimensions=selectedColumns, color=cluster_assignments)        
+        pair_plot.update_layout(title='K-means Clustering - Pair Plots')
+        cluster_profile = pd.DataFrame(cluster_centers, columns=selectedData.columns)
+        bar_plot = px.bar(cluster_profile)
+        bar_plot.update_layout(title='K-means Clustering - Cluster Profile')
+        fig.append(bar_plot)
+        fig.append(pair_plot)
+    """.format(inputData=inputData, selectedColumns=selectedColumns, numClusters=numClusters)
     return fig, statistics
+
+
 
 
 def classification_SVM(inputData, selectedColumns, targetColumn,kernel):
@@ -87,7 +121,48 @@ def classification_SVM(inputData, selectedColumns, targetColumn,kernel):
         AUC_Plot.update_layout(title='SVM Classification - Area Under Curve Plot')
         fig.append(AUC_Plot)
 
-    return fig, statistics
+    # Export Script 
+    exportScript = """
+    fig = []
+    selectedData = inputData[selectedColumns]
+
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(selectedData, inputData[targetColumn], test_size=0.2, random_state=42)
+    # Create and train SVM Classifier 
+    classifier = svm.SVC(kernel=kernel) 
+    classifier.fit(X_train, y_train)
+    # Predict labels on the test data
+    y_pred = classifier.predict(X_test)
+    # Calculate confusion matrix and classification report
+    cm = confusion_matrix(y_test, y_pred)
+    # fig.append(cm)
+    class_report = classification_report(y_test, y_pred, output_dict=True)
+
+    # Create data analysis HTML report 
+    statistics = html.Div([
+        html.H4("Classification Statistics:"),
+        html.P(f"Precision: {class_report['weighted avg']['precision']}"),
+        html.P(f"Recall: {class_report['weighted avg']['recall']}"),
+        html.P(f"F1-Score: {class_report['weighted avg']['f1-score']}"),
+        html.P(f"Support: {class_report['weighted avg']['support']}")
+      ])
+        # Create the confusion matrix plot
+    figure = generateConfusionMatrix(cm,class_labels=classifier.classes_)
+    figure.update_layout(title='SVM Classification - Confusion Matrix')
+    fig.append(figure)
+    
+    if(len(classifier.classes_ == 2)):
+        y_pred_bin = label_binarize(y_pred, classes=classifier.classes_)
+        y_test_bin = label_binarize(y_test, classes=classifier.classes_)
+        AUC_Plot =  generateAUC(y_test_bin,y_pred_bin)
+        AUC_Plot.update_layout(title='SVM Classification - Area Under Curve Plot')
+        fig.append(AUC_Plot)
+    """.format(inputData=inputData, selectedColumns=selectedColumns, targetColumn=targetColumn, kernel=kernel)
+
+
+
+
+    return fig, statistics,
 
 
 def generateAUC(y_test, y_pred):
